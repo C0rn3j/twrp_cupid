fd5f7177dc1bb6c6a5dcce3cf4d667ff898f4af4
Martin Rys <martin@rys.pw>
2022-05-13 22:42:50 +0200

    .

--- device.mk
+++ device.mk
@@ -15,17 +15,11 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-impl-qti \
+    android.hardware.boot@1.0-impl-qti.recovery \
     android.hardware.boot@1.0-service \
     bootctrl.taro \
     bootctrl.taro.recovery
-
-#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-#    bootctrl.taro \
-#    libgptutils \
-#    libz \
-#    libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
